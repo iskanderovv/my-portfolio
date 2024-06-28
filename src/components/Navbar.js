0c09eebbacd4c7c3bce9670d6055805ffac71131
Akbar Iskanderov <iskanderovofficial@gmail.com>
2024-06-29 00:40:09 +0500

    fixed some bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -78,7 +78,7 @@ function NavBar() {
                 <AiOutlineFundProjectionScreen
                   style={{ marginBottom: "2px" }}
                 />{" "}
-                Projects
+                My Projects
               </Nav.Link>
             </Nav.Item>
 
